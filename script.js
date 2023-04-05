45ec7a0548fb76efccb04f66c5ea55764c28047f
mateorinland <121818729+mateorinland@users.noreply.github.com>
2023-04-05 10:02:37 -0300

    Aria-expanded attribute

--- script.js
+++ script.js
@@ -3,5 +3,11 @@ const shareContainer = document.querySelector(".share-container");
 
 shareButton.addEventListener("click", () => {
   shareContainer.classList.toggle("toggle");
-  shareButton.setAttribute("aria-expanded", `${!(shareButton.getAttribute('aria-expanded'))}`)
+  let expandedValue = shareButton.getAttribute("aria-expanded");
+  if(expandedValue == "true") {
+    expandedValue = "false";
+  } else {
+    expandedValue = "true";
+  }
+  shareButton.setAttribute("aria-expanded", expandedValue);
 });
